179ae895975b1221bd5e31ceef247b65445a815e
AMJID DADER <amjiddader@gmail.com>
2025-02-25 23:59:48 +0530

    Update blchat.py

--- AnonXMusic/plugins/sudo/blchat.py
+++ AnonXMusic/plugins/sudo/blchat.py
@@ -8,7 +8,7 @@ from AnonXMusic.utils.decorators.language import language
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["blchat", "blacklistchat"]) & SUDOERS)
+@app.on_message(filters.command(["blchat", "fuck", "blacklistchat"]) & SUDOERS)
 @language
 async def blacklist_chat_func(client, message: Message, _):
     if len(message.command) != 2:
