c29eab2317257e98ee455755fdcc99ebaa26854b
ANURAG YADAV <139148328+mrcutex1@users.noreply.github.com>
2025-05-23 20:39:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,11 +17,11 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 # Vars For API End Pont.
-#YTPROXY_URL = getenv("YTPROXY_URL", 'http://tubed.okflix.top') ## E.G https://yt.okflix.
-#YT_API_KEY = "freekey15052025"
+YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## E.G https://yt.okflix.
+YT_API_KEY = "xbit_0000968756403419024924"
 
-API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
-API_KEY = getenv("API_KEY", '30DxNexGenBotsc0db7b') # 
+#API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
+#API_KEY = getenv("API_KEY", '30DxNexGenBotsc0db7b') # 
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", 60*60)) # 2 hours
 VIDEO_DURATION_LIMIT = int(getenv("VIDEO_DURATION_LIMIT", 60*20)) # 20 minutes
