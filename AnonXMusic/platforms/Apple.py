57d83ea9ab39f4ba8af433f4a32d8b95daf43d96
ANURAG YADAV <139148328+mrcutex1@users.noreply.github.com>
2025-12-24 08:19:54 +0530

    Update Apple.py

--- AnonXMusic/platforms/Apple.py
+++ AnonXMusic/platforms/Apple.py
@@ -3,8 +3,7 @@ from typing import Union
 
 import aiohttp
 from bs4 import BeautifulSoup
-from youtubesearchpython.__future__ import VideosSearch
-
+from ytSearch import VideosSearch
 
 class AppleAPI:
     def __init__(self):
